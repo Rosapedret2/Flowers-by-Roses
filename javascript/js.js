fdfee6040664f6eeccbcc3be004779e4e09a28df
rosa-rubio <rosamrubio@gmail.com>
2023-06-07 12:01:50 +0200

    favourite button fuction done!

--- javascript/js.js
+++ javascript/js.js
@@ -8,9 +8,17 @@ fetch (url)
 const curiousFactsWindow = document.getElementById('curious-facts')
 
 const showData = (data) => {
-    
-    curiousFactsWindow.textContent = data.text ;
+    curiousFactsWindow.textContent = data.text;
 }
 
+const favouritesAdd = document.getElementById('favourites-button')
+const listItems = document.getElementById('list')
+
+favouritesAdd.addEventListener("click", function () {
+    let favouriteItem = document.createElement("li");
+    favouriteItem.classList.add("favourite-item");
+    favouriteItem.textContent = curiousFactsWindow.textContent;
+    listItems.appendChild(favouriteItem);
+})
 
  
